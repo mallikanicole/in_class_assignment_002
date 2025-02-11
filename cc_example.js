a8f33173f6a5c53bdafc2f22e48dbc53c1685e4b
mallikanicole <malmichael23@gmail.com>
2025-02-11 16:31:23 -0500

    task 3 running total

--- cc_example.js
+++ cc_example.js
@@ -11,19 +11,32 @@ console.log("OG list",products)
 
 //Task 2
 
-function applyDiscount(products,discountRate){
-    return products.map(product=>({
-        ...product,
-        price:product.price-(product.price*discountRate)//overwrite price
-    }
-    ))
-};//map applies to every single item in array
-console.log("New List:", applyDiscount(products,.1))
-// function sum3(x,y,z){
-//     return x+y+z;
+// function applyDiscount(products,discountRate){
+//     return products.map(product=>({
+//         ...product,
+//         price:product.price-(product.price*discountRate)//overwrite price
+//     }
+//     ))
+// };//map applies to every single item in array
+// console.log("New List:", applyDiscount(products,.1))
+
+
+
+
+//...example
+// // function sum3(x,y,z){
+// //     return x+y+z;
 // }
 // let numbers=[1,2,,4]
 
 // console.log(sum3(...numbers));//elipse of numbers separates out numbers inside, ... also works with objects. use ... for objects in an array to be sed in function.
 
 
+//Task 3
+
+let sales=[250,400,150,]
+
+function calculateTotalRevenue(sales){
+    return sales.reduce((total,sale)=>total+sale,0)//0 is starting point,start adding at 0
+};//start runningtotal (sales, runningTotal) if already have running total
+console.log(`Total Revenue 2024:$${calculateTotalRevenue}`)
