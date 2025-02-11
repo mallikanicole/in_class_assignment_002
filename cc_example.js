2c1d9078b282a474cfd9ccf188f6eb93fca31731
mallikanicole <malmichael23@gmail.com>
2025-02-11 16:06:51 -0500

    used ctrl and / to comment out code

--- cc_example.js
+++ cc_example.js
@@ -3,7 +3,10 @@ let products=[
     {name:"Phone", price:800, category:"Electronics"},
     {name:"Desk Chair", price:150,category:"Furniture"}
 ];
-function getProductsByCategory(products, category){
-    return products.filter(product=>product.category===category);//filter grabs things inside the array. using callback to filter category
-}
-console.log(getProductsByCategory(products,"Electronics"));
+// function getProductsByCategory(products, category){
+//     return products.filter(product=>product.category===category);//filter grabs things inside the array. using callback to filter category
+// }
+// console.log(getProductsByCategory(products,"Electronics")); use control and forward slash to comment out code
+
+//Task 2
+
