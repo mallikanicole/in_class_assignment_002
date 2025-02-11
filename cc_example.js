fad3beb52b5e9ebb22f420756958d379070010c4
mallikanicole <malmichael23@gmail.com>
2025-02-11 16:19:00 -0500

    apply discount to array

--- cc_example.js
+++ cc_example.js
@@ -10,3 +10,19 @@ let products=[
 
 //Task 2
 
+function applyDiscount(products,discountRate){
+    return products.map(product=>({
+        ...product,
+        price:product.price-(product.price*discountRate)//overwrite price
+    }
+    ))
+};//map applies to every single item in array
+console.log(applyDiscount(products,.1))
+// function sum3(x,y,z){
+//     return x+y+z;
+// }
+// let numbers=[1,2,,4]
+
+// console.log(sum3(...numbers));//elipse of numbers separates out numbers inside, ... also works with objects. use ... for objects in an array to be sed in function.
+
+
